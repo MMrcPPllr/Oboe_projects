0fc69680312618f2e221cc48ea71c5f7e6737886
MMrcPPllr <marcopallaro99@gmail.com>
2023-05-10 16:26:10 +0200

    work

--- test_matrici_oboe.m
+++ test_matrici_oboe.m
@@ -1,12 +1,15 @@
-%Jr = 5.7e-5;
-%Jp = 3.3e-5;
-Jr = 5.6e-5;
-Jp = 3.3e-5;
+% % % % % % % Jr = 5.7e-5;
+% % % % % % % Jp = 3.3e-5;
+% % % % % % % % Jr = 5.6e-5;
+% % % % % % % % Jp = 3.3e-5;
+% % % % % % % 
+% % % % % % % 
+% % % % % % % 
+% % % % % % % lr = r;
+% % % % % % % lp = 0.5*Lp;
 
-
-
-lr = r;
-lp = 0.5*Lp;
+%%
+syms Jr Jp lr lp br bp g mp km Rm
 
 M = [Jr + mp*lr^2 -mp*lr*lp;
     -mp*lr*lp Jp+mp*lp^2];
@@ -19,7 +22,24 @@ K = [0 0;
 
 
 A = [zeros(2) eye(2)
-    -inv(M)*K -inv(M)*C]
+    -inv(M)*K -inv(M)*C];
 
 B = [0 0; 0 0; inv(M)];
 B = B(:,1);
+
+
+
+
+
+%test = -(br*(mp*lp^2 + Jp))/(Jr*mp*lp^2 + Jp*mp*lr^2 + Jp*Jr);
+
+% inv_jm = (mp*lp^2 + Jp)/(Jr*mp*lp^2 + Jp*mp*lr^2 + Jp*Jr);
+% test = (km/Rm)*inv_jm*(km^2/Rm)
+
+
+
+B = km * B / Rm;
+A(3,3) = A(3,3) - km*km/Rm*B(3);
+A(4,3) = A(4,3) - km*km/Rm*B(4);
+
+
