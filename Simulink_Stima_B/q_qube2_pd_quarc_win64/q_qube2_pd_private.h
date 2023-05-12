76986bbb55f19a75768a6f1d2b3fd700b6423943
MMrcPPllr <marcopallaro99@gmail.com>
2023-05-12 18:10:59 +0200

    work

---
+++ Simulink_Stima_B/q_qube2_pd_quarc_win64/q_qube2_pd_private.h
@@ -0,0 +1,36 @@
+/*
+ * q_qube2_pd_private.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "q_qube2_pd".
+ *
+ * Model version              : 4.1
+ * Simulink Coder version : 9.4 (R2020b) 29-Jul-2020
+ * C source code generated on : Fri May 12 17:15:13 2023
+ *
+ * Target selection: quarc_win64.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_q_qube2_pd_private_h_
+#define RTW_HEADER_q_qube2_pd_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+#include "zero_crossing_types.h"
+#include "q_qube2_pd.h"
+
+/* A global buffer for storing error messages (defined in quanser_common library) */
+EXTERN char _rt_error_message[512];
+void q_qube2_pd_output0(void);
+void q_qube2_pd_update0(void);
+void q_qube2_pd_output2(void);
+void q_qube2_pd_update2(void);         /* private model entry point functions */
+extern void q_qube2_pd_derivatives(void);
+
+#endif                                 /* RTW_HEADER_q_qube2_pd_private_h_ */
